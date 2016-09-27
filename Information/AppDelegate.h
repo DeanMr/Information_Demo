d46b29989b9fffbcbdf7057e74b1362fdd2d370b
UBDean <deanlzwei@gmail.com>
2016-09-27 15:15:25 +0800

    Initial Commit

---
+++ Information/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Information
+//
+//  Created by 云宝 Dean on 2016/9/27.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Information/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Information
+//
+//  Created by 云宝 Dean on 2016/9/27.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
